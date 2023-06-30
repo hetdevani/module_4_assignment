50ce45321cc254e5f363766e47fcfdfbc8704c16
hetdevani <devanihet@gmail.com>
2023-06-29 21:11:51 -0700

    complated module 4 assignment

---
+++ q4..cpp
@@ -0,0 +1,56 @@
+// WAP to create simple calculator using class
+
+#include <iostream>
+using namespace std;
+
+class calculater                                                                            
+{
+
+  public:
+
+  void sum()
+
+  {
+
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator: +, -, *, /: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch (op)
+    {
+
+    case '+':
+      cout << num1 << " + " << num2 << " = " << num1 + num2;
+      break;
+
+    case '-':
+      cout << num1 << " - " << num2 << " = " << num1 - num2;
+      break;
+
+    case '*':
+      cout << num1 << " * " << num2 << " = " << num1 * num2;
+      break;
+
+    case '/':
+      cout << num1 << " / " << num2 << " = " << num1 / num2;
+      break;
+
+    default:
+      cout << "Error! operator is not correct";
+      break;
+
+    }
+  }
+};
+
+int main()
+{
+  calculater obj;
+
+  obj.sum();
+}
